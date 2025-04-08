33ee22de41e53a009d860e053b356dee27092943
Vardhan velamakanni <159052557+VardhanVelamakanni@users.noreply.github.com>
2025-04-08 10:46:59 +0530

    commit

--- idp/src/components/ui/hero/movingdemo.tsx
+++ idp/src/components/ui/hero/movingdemo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import _React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "../hero/infinite-moving-cards";
 
 export function InfiniteMovingCardsDemo() {
